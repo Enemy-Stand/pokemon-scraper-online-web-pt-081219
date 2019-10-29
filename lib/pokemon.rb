12747a2a6663483bd508d31c4ed074c3bffac5eb
Joseph Sesti <sestijoey63@gmail.com>
2019-10-29 21:03:08 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -23,5 +23,14 @@ class Pokemon
     sql = "SELECT * FROM pokemon WHERE id = ? LIMIT 1"
     result = db.execute(sql, id)[0]
     Pokemon.new(id: result[0], name: result[1], type: result[2], db: db)
+
+    db[:conn].execute(sql, name, type, id)
+    @id = db[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0][0]
+  end
+  
+  def self.find(id)
+    sql = "SELECT * FROM pokemon WHERE id = ? LIMIT 1"
+    result = DB[:conn].execute(sql, id)[0]
+    Pokemon.new(result[0], result[1], result[2], result[3])
   end
 end
